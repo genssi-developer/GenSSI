5d1d1bf4c63f82754cd9a61a8d57ebb0ebb88526
Thomas S. Ligon <tom@thomassligon.info>
2016-02-19 16:56:52 +0100

    First Github version

---
+++ Docu/output/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['genssi_202_2e0_20general_20documentation',['GenSSI 2.0 General Documentation',['../index.html',1,'']]]
+];
---
+++ Docu/output/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "cgm",
+  1: "g",
+  2: "gm",
+  3: "cgm"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions",
+  3: "Pages"
+};
+
