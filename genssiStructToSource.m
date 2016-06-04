cb1fc23bac57becbbf9cbc6dcdbd402d05c00f8f
Thomas S. Ligon <tom@thomassligon.info>
2016-06-04 11:47:02 +0200

    minor edits

--- genssiStructToSource.m
+++ genssiStructToSource.m
@@ -1,4 +1,4 @@
-function genSsiStructToSource(model)
+function genssiStructToSource(model)
     % genSsiStructToSource converts a model definition (struct) to a
     %  source format (Matlab function file) ans saves the results in the
     %  examples directory.
@@ -22,8 +22,14 @@ function genSsiStructToSource(model)
     end
     fprintf(fileID,['\t' strSyms '\n']);
     strSyms = 'syms';
-    for iStr = 1:length(model.Par)
-        strSyms = [strSyms ' ' char(model.Par(iStr))];
+    if isfield(model,'P')
+        for iStr = 1:length(model.P)
+            strSyms = [strSyms ' ' char(model.P(iStr))];
+        end
+    else
+        for iStr = 1:length(model.Par)
+            strSyms = [strSyms ' ' char(model.Par(iStr))];
+        end
     end
     fprintf(fileID,['\t' strSyms '\n']);
     fprintf(fileID,['\t' 'model.Name = ''' model.Name ''';\n']);
@@ -45,17 +51,35 @@ function genSsiStructToSource(model)
     else
         strMat = '\tmodel.G = [';
         if ~isempty(model.G)
-            strMat = [strMat char(model.G(1))];
-        end
-        if length(model.G)>1
-            for iStr = 2:length(model.G)
-                strMat = [strMat ',' char(model.G(iStr))];
+            for iRow = 1:size(model.G,1)
+                if iRow>1
+                    strMat = [strMat ';'];
+                end
+                for iCol = 1:size(model.G,2)
+                    if iCol>1
+                        strMat = [strMat ','];
+                    end
+                    strMat = [strMat char(model.G(iRow,iCol))];
+                end
             end
         end
         strMat = [strMat '];\n'];
     end
     fprintf(fileID,strMat);
     fprintf(fileID,['\t' 'model.Noc = ' num2str(model.Noc) ';\n']);
+    strMat = '\tmodel.P = [';
+    if isfield(model,'P')
+        if ~isempty(model.Par)
+            strMat = [strMat char(model.Par(1))];
+        end
+        if length(model.Par)>1
+            for iStr = 2:length(model.Par)
+                strMat = [strMat ',' char(model.Par(iStr))];
+            end
+        end
+    end
+    strMat = [strMat '];\n'];
+    fprintf(fileID,strMat);
     strMat = '\tmodel.Par = [';
     if ~isempty(model.Par)
         strMat = [strMat char(model.Par(1))];
@@ -70,11 +94,11 @@ function genSsiStructToSource(model)
     fprintf(fileID,['\t' 'model.Npar = ' num2str(model.Npar) ';\n']);     
     strMat = '\tmodel.IC = [';
     if ~isempty(model.IC)
-        strMat = [strMat char(model.IC(1))];
+        strMat = [strMat char(sym(model.IC(1)))];
     end
     if length(model.IC)>1
         for iStr = 2:length(model.IC)
-            strMat = [strMat ',' char(model.IC(iStr))];
+            strMat = [strMat ',' char(sym(model.IC(iStr)))];
         end
     end
     strMat = [strMat '];\n'];
