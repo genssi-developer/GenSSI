b2aea0b07886e6931818fbebca4bf35a18e72f4d
Thomas S. Ligon <tom@thomassligon.info>
2016-12-04 19:22:37 +0100

    Add files via upload
    minor edits

--- runAll.m
+++ runAll.m
@@ -1,9 +1,25 @@
 function runAll()
     genssiMain('Arabidopsis');
+%    genssiMain('ArabidopsisPoly');
+%    genssiMain('Bilirubin1');
+%     genssiMain('Bilirubin2');
+%    genssiMain('Cholesterol1');
+%    genssiMain('Cholesterol2');
+%    genssiMain('Degradation');
+%    genssiMain('DegradationPoly');
     genssiMain('Glycolysis');
     genssiMain('Goodwin');
+%    genssiMain('GoodwinPoly');
     genssiMain('HighDimNonLin');
+%    genssiMain('HIV');
+%    genssiMain('JAK_STAT');
     genssiMain('NFkB');
-    genssiMain('pharmacokinetics');
-    genssiMain('pharmacokinetics2');
+%    genssiMain('NGF_Erk');
+    genssiMain('Pharmacokinetics');
+%    genssiMain('PharmacokineticsPoly');
+%    genssiMain('Thyroid1');
+%    genssiMain('Thyroid2');
+%    genssiMain('Transfection1');
+%    genssiMain('Transfection2');
+%    genssiMain('Transfection3');
 end
