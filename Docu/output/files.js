7d498abd76fe8a3a8551805a726933103a34b5f2
genssi-developer <thomassligon@hotmail.com>
2017-02-02 14:56:55 +0100

    updating reference manual

--- Docu/output/files.js
+++ Docu/output/files.js
@@ -1,25 +1,25 @@
 var files =
 [
-    [ "amiciStructToSource.m", "amici_struct_to_source_8m_source.html", null ],
+    [ "config", "dir_9f351d46ce3cc29445a41dc3a31e6919.html", "dir_9f351d46ce3cc29445a41dc3a31e6919" ],
+    [ "amiciStructToSource.m", "amici_struct_to_source_8m.html", "amici_struct_to_source_8m" ],
     [ "convert2PolySys.m", "convert2_poly_sys_8m_source.html", null ],
     [ "genssiComputeLieDerivatives.m", "genssi_compute_lie_derivatives_8m.html", "genssi_compute_lie_derivatives_8m" ],
     [ "genssiComputeReducedTableau.m", "genssi_compute_reduced_tableau_8m.html", "genssi_compute_reduced_tableau_8m" ],
     [ "genssiComputeTableau.m", "genssi_compute_tableau_8m.html", "genssi_compute_tableau_8m" ],
     [ "genssiFromAmici.m", "genssi_from_amici_8m.html", "genssi_from_amici_8m" ],
-    [ "genssiJacobian2D.m", "genssi_jacobian2_d_8m.html", "genssi_jacobian2_d_8m" ],
     [ "genssiMain.m", "genssi_main_8m.html", "genssi_main_8m" ],
+    [ "genssiMultiExperiment.m", "genssi_multi_experiment_8m.html", "genssi_multi_experiment_8m" ],
     [ "genssiOrderTableau.m", "genssi_order_tableau_8m.html", "genssi_order_tableau_8m" ],
     [ "genssiRemoveZeroColumns.m", "genssi_remove_zero_columns_8m.html", "genssi_remove_zero_columns_8m" ],
-    [ "genssiRemoveZeroElements.m", "genssi_remove_zero_elements_8m_source.html", null ],
     [ "genssiRemoveZeroElementsC.m", "genssi_remove_zero_elements_c_8m.html", "genssi_remove_zero_elements_c_8m" ],
     [ "genssiRemoveZeroElementsR.m", "genssi_remove_zero_elements_r_8m.html", "genssi_remove_zero_elements_r_8m" ],
     [ "genssiRemoveZeroRows.m", "genssi_remove_zero_rows_8m.html", "genssi_remove_zero_rows_8m" ],
     [ "genssiReportInputs.m", "genssi_report_inputs_8m.html", "genssi_report_inputs_8m" ],
     [ "genssiReportResults.m", "genssi_report_results_8m.html", "genssi_report_results_8m" ],
     [ "genssiStartup.m", "genssi_startup_8m.html", "genssi_startup_8m" ],
-    [ "genSsiStructToSource.m", "gen_ssi_struct_to_source_8m.html", "gen_ssi_struct_to_source_8m" ],
-    [ "genssiTableauImage.m", "genssi_tableau_image_8m_source.html", null ],
+    [ "genssiStructToSource.m", "genssi_struct_to_source_8m.html", "genssi_struct_to_source_8m" ],
+    [ "genssiTableauImage.m", "genssi_tableau_image_8m.html", "genssi_tableau_image_8m" ],
     [ "genssiToAmici.m", "genssi_to_amici_8m.html", "genssi_to_amici_8m" ],
     [ "genssiToPolynomial.m", "genssi_to_polynomial_8m.html", "genssi_to_polynomial_8m" ],
-    [ "runAll.m", "run_all_8m_source.html", null ]
+    [ "genssiTransformation.m", "genssi_transformation_8m.html", "genssi_transformation_8m" ]
 ];
---
+++ Docu/output/genssi_multi_experiment_8m.js
@@ -0,0 +1,4 @@
+var genssi_multi_experiment_8m =
+[
+    [ "genssiMultiExperiment", "genssi_multi_experiment_8m.html#a99c14a4d68fa9a17610a21cc6ead7e30", null ]
+];
