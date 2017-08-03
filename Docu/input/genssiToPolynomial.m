6597829fc39d41d270ad701da5d093dc69a98637
genssi-developer <thomassligon@hotmail.com>
2017-08-03 11:31:54 +0200

    Add files via upload
    updated naming convention for Polynomials
    updated refman

--- Docu/input/genssiToPolynomial.m
+++ Docu/input/genssiToPolynomial.m
@@ -1,8 +1,8 @@
 function genssiToPolynomial (modelNameIn,modelNameOut)
     % genssiToPolynomial converts a GenSSI model to polynomial form.
     % It reads the input model, converts to polynomial form, and creates
-    % an output model as a MATLAB function modelNameOut.m and as a
-    % MATLAB file modelnameOut.mat, both in the Examples folder.
+    % an output model as a Matlab function modelNameOut.m and as a
+    % Matlab file modelnameOut.mat, both in the Examples folder.
     %
     % Parameters:
     %  modelNameIn: the name of the input model (a string)
@@ -17,7 +17,7 @@ function genssiToPolynomial (modelNameIn,modelNameOut)
     runModel = str2func(modelNameIn);
     modelIn=runModel();
     model.Name=modelNameOut;
-    [model.X,model.F,model.IC] = convert2PolySys(transpose(modelIn.X),transpose(modelIn.F),transpose(modelIn.IC));
+    [model.X,model.F,model.IC] = genssiPolySys(transpose(modelIn.X),transpose(modelIn.F),transpose(modelIn.IC));
     model.X = transpose(model.X);
     model.F = transpose(model.F);
     model.IC = transpose(model.IC);
@@ -25,13 +25,8 @@ function genssiToPolynomial (modelNameIn,modelNameOut)
     model.Neq = length(model.X);
     model.G = sym(zeros(1,length(model.X)));
     model.Noc = 0;
-%     model.H = model.X;
-%     model.Nobs = length(model.H);
-    model.H = modelIn.H;
-    model.Nobs = modelIn.Nobs;
-    if isfield(modelIn,'P')
-        model.P = modelIn.P;
-    end
+    model.H = model.X;
+    model.Nobs = length(model.H);
     model.Par = modelIn.Par;
     model.Npar = length(model.Par);
     save(fullfile(GenSSIDir,'Examples',[model.Name '.mat']),'model');
