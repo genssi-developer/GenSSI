9699d9c77e72280e36c8ae9f9a8c55d3dc7e1f4a
genssi-developer <thomassligon@hotmail.com>
2017-08-13 18:18:16 +0200

    Add files via upload
    minor edits

--- Examples/AMICI/ArabidopsisAmici.m
+++ Examples/AMICI/ArabidopsisAmici.m
@@ -1,4 +1,4 @@
-function model = ArabidopsisAmici()
+function model = Arabidopsis_syms()
 	syms x1 x2 x3 x4 x5 x6 x7
 	syms p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 p12 p13 p14 p15 p16 p17 p18 p19 p20 p21 p22 p23 p24 p25 p26 p27
 	model.sym.x = [x1,x2,x3,x4,x5,x6,x7];
