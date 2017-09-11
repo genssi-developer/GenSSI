c3e08fbb866f32d8c1a904ca24264d54a5f66062
JanHasenauer <jan.hasenauer@gmx.de>
2017-09-12 00:54:35 +0200

    update of BIOMD0000000010

--- Examples/BIOMD0000000010/runBIOMD0000000010.m
+++ Examples/BIOMD0000000010/runBIOMD0000000010.m
@@ -1,24 +1,24 @@
-% runBIOMD0000000003 runs the structural identifiability analysis for the 
-% model of the mitotic oscillator described in
+% runBIOMD0000000010 runs the structural identifiability analysis for the 
+% model of the MAP kinase cascade described in
 % 
-%    Goldbeter A (1991). A minimal cascade model for the mitotic
-%    oscillator involving cyclin and cdc2 kinase. Proc. Natl. Acad. Sci. 
-%    U.S.A. 88(20): 9107-9111.
+%    Kholodenko BN (2000). Negative feedback and ultrasensitivity can 
+%    bring about oscillations in the mitogen-activated protein kinase 
+%    cascades. Eur. J. Biochem., 267(6): 1583-1588.
 %
 % The model has been downloaded from the BioModels Database 
-% (http://biomodels.caltech.edu/BIOMD0000000003) as an SBML file. This SBML
+% (http://biomodels.caltech.edu/BIOMD0000000010) as an SBML file. This SBML
 % file is converted in the GenSSI format before analysis using the libSBML
 % see (http://sbml.org/Software/libSBML/Downloading_libSBML). This file is
 % extended by observables and renamed.
 
 % Model name
-modelName = 'BIOMD0000000003';
+modelName = 'BIOMD0000000010';
 
 % Import of SBML model
 ODE = SBMLode([modelName '.xml']);
 
 % Definition of observable
-ODE.observable = ODE.state;
+ODE.observable = ODE.state([2,5,8]);
 
 % Writing of model to AMICI format
 % (which is consistent with GenSSI)
@@ -28,5 +28,5 @@ ODE.writeAMICI(modelName);
 movefile([modelName '_syms.m'],[modelName '.m']);
 
 % Structural identifiability analysis (for a subset of the model parameters)
-genssiMain(modelName,8,ODE.parameter([4,6,9,13]));
+genssiMain(modelName,6,ODE.parameter([1,5,11,13,19,21]));
 
