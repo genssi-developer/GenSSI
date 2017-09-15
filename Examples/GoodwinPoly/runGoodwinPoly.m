3c037a95b07db274899a71185c09bcfd5bd668fb
JanHasenauer <jan.hasenauer@gmx.de>
2017-09-15 14:35:41 +0200

    Update of GoodwinPoly

--- Examples/GoodwinPoly/runGoodwinPoly.m
+++ Examples/GoodwinPoly/runGoodwinPoly.m
@@ -1,4 +1,19 @@
-Name = 'Goodwin';
-copyfile(fullfile('..',Name,[Name,'.m']),[Name,'.m']);
+% runGoodwinPoly runs the structural identifiability analysis for the 
+% model of the Goodwin oscillator as introduced by
+% 
+%    Goodwin, B.C. (1965). Oscillatory behavior in enzymatic control
+%              processes, Adv. Enzyme Regul. (3), 425-428. 
+%
+% To simplify the symbolic calculations, the model is converted to
+% polynomial form, meaning that the vector field of the autonomous 
+% dynamics (f) and the control vector (g) are polynomial. To achieve this,
+% the dimensionality of the model is increased.
+
+% Copy Goodwin.m to this folder
+copyfile(fullfile('..','Goodwin','Goodwin.m'),'Goodwin.m');
+
+% Transform model to polynomial
 genssiToPolynomial('Goodwin','GoodwinPoly');
+
+% Structural identifiability analysis (for all parameters)
 genssiMain('GoodwinPoly',4);
