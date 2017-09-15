56ddbfaba7223f211da53f5d0913658a1da6dfc8
JanHasenauer <jan.hasenauer@gmx.de>
2017-09-15 15:57:54 +0200

    Update of PharmacokineticsPoly

--- Examples/PharmacokineticsPoly/runPharmacokineticsPoly.m
+++ Examples/PharmacokineticsPoly/runPharmacokineticsPoly.m
@@ -1,4 +1,20 @@
-Name = 'Pharmacokinetics';
-copyfile(fullfile('..',Name,[Name,'.m']),[Name,'.m']);
+% runPharmacokinetics runs the structural identifiability analysis for the 
+% model of the pharmacokinetics of glucose-oxidase introduced by
+% 
+%    Demignot, S. and Domurado, D. (1987). Effect of prosthetic 
+%    sugar groups on the pharmacokinetics of glucose-oxidase, 
+%    Drug. Des. Deliv., 1, 333-348.
+%
+% To simplify the symbolic calculations, the model is converted to
+% polynomial form, meaning that the vector field of the autonomous 
+% dynamics (f) and the control vector (g) are polynomial. To achieve this,
+% the dimensionality of the model is increased.
+
+% Copy Pharmacokinetics.m to this folder
+copyfile(fullfile('..','Pharmacokinetics','Pharmacokinetics.m'),'Pharmacokinetics.m');
+
+% Transform model to polynomial
 genssiToPolynomial('Pharmacokinetics','PharmacokineticsPoly');
+
+% Structural identifiability analysis (for all parameters)
 genssiMain('PharmacokineticsPoly',3);
