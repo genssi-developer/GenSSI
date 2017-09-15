53352eda1c9bc9ddfc640ee64a66cf3fe60e20ed
JanHasenauer <jan.hasenauer@gmx.de>
2017-09-15 13:29:31 +0200

    Update of DegradtionPoly

--- Examples/DegradationPoly/runDegradationPoly.m
+++ Examples/DegradationPoly/runDegradationPoly.m
@@ -1,4 +1,15 @@
-Name = 'Degradation';
-%copyfile(fullfile('..',Name,[Name,'.m']),[Name,'.m']);
+% runDegradation runs the structural identifiability analysis for a 
+% simple model for protein synthesis and enzymatic degradation.
+% To simplify the symbolic calculations, the model is converted to
+% polynomial form, meaning that the vector field of the autonomous 
+% dynamics (f) and the control vector (g) are polynomial. To achieve this,
+% the dimensionality of the model is increased.
+
+% Copy Arabidopsis.m to this folder
+copyfile(fullfile('..','Degradation','Degradation.m'),'Degradation.m');
+
+% Transform model to polynomial
 genssiToPolynomial('Degradation','DegradationPoly');
+
+% Structural identifiability analysis (for all parameters)
 genssiMain('DegradationPoly',4);
