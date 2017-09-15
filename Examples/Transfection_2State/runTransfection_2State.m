5d230825709a7c64d8a94258cc76067b50b21696
JanHasenauer <jan.hasenauer@gmx.de>
2017-09-15 22:40:00 +0200

    update of transfection1
    the example has been renamed to clarify differences. furthermore, the example has been reformatted

---
+++ Examples/Transfection_2State/runTransfection_2State.m
@@ -0,0 +1,8 @@
+% runTransfection_2State performs the structural identifiability analysis
+% for the model for mRNA transfection introduced by
+% 
+%    Leonhardt et al. (2013). Single-cell mRNA transfection 
+%    studies: delivery, kinetics and statistics by numbers, 
+%    Nanomedicine: Nanotechnology, Biology and Medicine. 10(4):679-88.
+
+genssiMain('Transfection_2State',3);
