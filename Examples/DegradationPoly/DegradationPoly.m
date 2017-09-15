912fb156c280407d0b956222695a0f71f853f6ed
JanHasenauer <jan.hasenauer@gmx.de>
2017-09-15 13:31:47 +0200

    Delete old results for DegradtionPoly
    the were outdated due to the update of the conversion routine

---
+++ Examples/DegradationPoly/DegradationPoly.m
@@ -0,0 +1,10 @@
+function model = DegradationPoly()
+	syms x xi1
+	syms k_syn k_deg_max K_deg
+	model.sym.x = [x,xi1];
+	model.sym.g = [];
+	model.sym.p = [k_syn,k_deg_max,K_deg];
+	model.sym.x0 = [0,1/K_deg];
+	model.sym.y = [x];
+	model.sym.xdot = [k_syn - k_deg_max*x*xi1,-xi1^2*(k_syn - k_deg_max*x*xi1)];
+end
