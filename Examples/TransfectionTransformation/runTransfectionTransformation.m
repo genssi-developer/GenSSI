9e1b715af7ac6c0913192e5120a5f48426e0c2b8
JanHasenauer <jan.hasenauer@gmx.de>
2017-09-16 02:43:32 +0200

    update of Transfection_4State and transformed version

--- Examples/TransfectionTransformation/runTransfectionTransformation.m
+++
@@ -1,6 +0,0 @@
-Name = 'Transfection2';
-copyfile(fullfile('..',Name,[Name,'.m']),[Name,'.m']);
-genssiTransformation('Transfection2','TransformationDefinition','TransfectionTransformation');
-% in the model M2_2, manually change (d3*enz0)/mRNA0 to enz0dmRNA0*d3
-% then run genssiMain
-% genssiMain('TransfectionTransformation',5);
