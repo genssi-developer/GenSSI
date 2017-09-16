72dd328dba04cd0e90491c92dc1ed2ed03ab7f92
JanHasenauer <jan.hasenauer@gmx.de>
2017-09-17 01:35:36 +0200

    fused genssiStartup and genssiInstall

--- genssiInstall.m
+++
@@ -1,9 +0,0 @@
-function genssiInstall
-    % Inlude GenSSI in MATLAB path
-    addpath(pwd);
-    addpath(genpath([pwd '/Auxiliary']));
-    addpath(genpath([pwd '/SBMLimporter']));
-    
-    % Generate file encoding user-specific defaults
-    copyfile([pwd '/Auxiliary/template_genssiUserSpecificDefaults'],'genssiUserSpecificDefaults.m');
-end
--- genssiStartup.m
+++ genssiStartup.m
@@ -1,9 +1,16 @@
-% genssiStartup adds all paths required for GenSSI.
-% It should be called at the beginning of a session.
+function genssiStartup
+    % genssiStartup adds all paths required for GenSSI. Furthermore, it
+    % generates the file genssiUserSpecificDefaults.m, providing
+    % user-specific defaults.
 
-GenSSIDir = fileparts(mfilename('fullpath'));
-addpath(GenSSIDir);
-addpath(fullfile(GenSSIDir,'Auxiliary'));
-addpath(fullfile(GenSSIDir,'SBMLimporter'));
-addpath(fullfile(GenSSIDir,'Examples'));
-addpath(fullfile(GenSSIDir,'Docu'));
+    % Inlude GenSSI in MATLAB path
+    GenSSIDir = fileparts(mfilename('fullpath'));
+    addpath(GenSSIDir);
+    addpath(fullfile(GenSSIDir,'Auxiliary'));
+    addpath(fullfile(GenSSIDir,'SBMLimporter'));
+    addpath(fullfile(GenSSIDir,'Examples'));
+    addpath(fullfile(GenSSIDir,'Docu'));
+
+    % Generate file encoding user-specific defaults
+    copyfile([pwd '/Auxiliary/template_genssiUserSpecificDefaults'],'genssiUserSpecificDefaults.m');
+end
