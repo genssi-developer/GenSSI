6cab6a7073117f65caf09cc1736ba2202f4a4622
genssi-developer <thomassligon@hotmail.com>
2017-09-16 14:05:57 +0200

    update name...
    name in TransfectionTransformation corrected

--- Examples/TransfectionTransformation/TransfectionTransformation.m
+++ Examples/TransfectionTransformation/TransfectionTransformation.m
@@ -6,5 +6,5 @@ function model = TransfectionTransformation()
 	model.sym.p = [d1,d2tmRNA0,d3,b,kTLtmRNA0,enz0dmRNA0];
 	model.sym.x0 = [1,0,enz0dmRNA0];
 	model.sym.y = [GFPnew];
-	model.sym.xdot = [- d1*mRNAnew - d2tmRNA0*enznew*mRNAnew,kTLtmRNA0*mRNAnew - GFPnew*b,enz0dmRNA0*d3 - d2tmRNA0*enznew*mRNAnew - d3*enznew];
+	model.sym.xdot = [- d1*mRNAnew - d2tmRNA0*enznew*mRNAnew,kTLtmRNA0*mRNAnew - GFPnew*b,(d3*enz0)/mRNA0 - d2tmRNA0*enznew*mRNAnew - d3*enznew];
 end
